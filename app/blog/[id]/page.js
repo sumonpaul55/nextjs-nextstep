e3eded2e40c8e4ff4528dc62109fec1c3b798f09
sumonpaul55 <sumpaul100@gmail.com>
2024-01-08 00:07:50 +0600

    notFound calling from root like notFound()

--- app/blog/[id]/page.js
+++ app/blog/[id]/page.js
@@ -1,7 +1,12 @@
+import { notFound } from 'next/navigation';
 import React from 'react'
 
 function EachBlogPage({ params }) {
     const { id } = params;
+
+    if (id > 2) {
+        notFound()//dynamic notfound page call
+    }
     return (
         <div>
             <h1 className='text-3xl'>The blog id is {id}</h1>
